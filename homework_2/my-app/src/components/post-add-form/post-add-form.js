787b06832f5080b6040d72d90ad55e86b8bf747c
Yevhenii Bershadskyi <yevhenii1709@gmail.com>
2021-02-22 23:54:18 +0200

    still work with forms

--- homework_2/my-app/src/components/post-add-form/post-add-form.js
+++ homework_2/my-app/src/components/post-add-form/post-add-form.js
@@ -2,21 +2,48 @@ import React, {Component} from 'react';
 
 import './post-add-form.css';
 
-export default class PostAddForm = ({onAdd}) => {
-    return (
-        <form className="buttom-panel d-flex">
-            <input
-                type="text"
-                placeholder="О чем вы думаете сейчас?"
-                className="form-control new-post-label"
-            />
-            <button
-                type="submit"
-                className="btn btn-outline-secodary"
-                onClick = {()=> onAdd('Hello')}>
-            Добавить</button>
-        </form>
-    )
-}
+export default class PostAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+        this.onValueChange = this.onValueChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
 
- PostAddForm;
+    onValueChange(e) {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        this.props.onAdd(this.state.text)
+        this.setState({
+            text: ''
+        });
+    }
+
+    render() {
+        return (
+            <form 
+                className="buttom-panel d-flex"
+                onSubmit ={this.onSubmit}>
+                <input
+                    type="text"
+                    placeholder="О чем вы думаете сейчас?"
+                    className="form-control new-post-label"
+                    onChange={this.onValueChange}
+                    value={this.state.text}
+                />
+                <button
+                    type="submit"
+                    className="btn btn-outline-secodary">
+                    Добавить
+                </button>
+            </form>
+        )
+    }
+}
